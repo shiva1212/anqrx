ed00379e9cd5873614db0eae8beafbb39628e00f
Nanda Reddy <nandaui503@gmail.com>
2019-03-17 20:48:21 -0400

    Sorting

--- src/Table.js
+++ src/Table.js
@@ -1,63 +1,365 @@
 import React from 'react';
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
-
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import DeleteIcon from '@material-ui/icons/Delete';
+import FilterListIcon from '@material-ui/icons/FilterList';
+import { lighten } from '@material-ui/core/styles/colorManipulator';
 import mockTableData from './mockTableData';
+
+const CustomTableCell = withStyles(theme => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+// const styles = theme => ({
+//   root: {
+//     width: '100%',
+//     marginTop: theme.spacing.unit * 3,
+//     overflowX: 'auto',
+//   },
+//   table: {
+//     minWidth: 700,
+//   },
+//   row: {
+//     '&:nth-of-type(odd)': {
+//       backgroundColor: theme.palette.background.default,
+//     },
+//   },
+// });
+
+
+function desc(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function stableSort(array, cmp) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = cmp(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map(el => el[0]);
+}
+
+function getSorting(order, orderBy) {
+  return order === 'desc' ? (a, b) => desc(a, b, orderBy) : (a, b) => -desc(a, b, orderBy);
+}
+
+const rows = [
+  { id: 'OrganizationName', numeric: false, disablePadding: true, label: 'OrganizationName' },
+  { id: 'PlanType', numeric: false, disablePadding: true, label: 'PlanType' },
+  { id: 'Product', numeric: false, disablePadding: true, label: 'Product' },
+  { id: 'Segment', numeric: false, disablePadding: true, label: 'Segment' },
+  { id: ' Geography', numeric: false, disablePadding: true, label: ' Geography' },
+];
+
+class EnhancedTableHead extends React.Component {
+  createSortHandler = property => event => {
+    this.props.onRequestSort(event, property);
+  };
+
+  render() {
+    const { onSelectAllClick, order, orderBy, numSelected, rowCount } = this.props;
+
+    return (
+      <TableHead>
+        <TableRow>
+          <TableCell >
+            
+          </TableCell>
+          {rows.map(
+            row => (
+              <TableCell
+                key={row.id}
+                align={row.numeric ? 'right' : 'left'}
+                padding={row.disablePadding ? 'none' : 'default'}
+                sortDirection={orderBy === row.id ? order : false}
+              >
+                <Tooltip
+                  title="Sort"
+                  placement={row.numeric ? 'bottom-end' : 'bottom-start'}
+                  enterDelay={300}
+                >
+                  <TableSortLabel
+                    active={orderBy === row.id}
+                    direction={order}
+                    onClick={this.createSortHandler(row.id)}
+                  >
+                    {row.label}
+                  </TableSortLabel>
+                </Tooltip>
+              </TableCell>
+            ),
+            this,
+          )}
+        </TableRow>
+      </TableHead>
+    );
+  }
+}
+
+EnhancedTableHead.propTypes = {
+ // numSelected: PropTypes.number.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  //onSelectAllClick: PropTypes.func.isRequired,
+  order: PropTypes.string.isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
+
+const toolbarStyles = theme => ({
+  root: {
+    paddingRight: theme.spacing.unit,
+  },
+  highlight:
+    theme.palette.type === 'light'
+      ? {
+          color: theme.palette.secondary.main,
+          backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+        }
+      : {
+          color: theme.palette.text.primary,
+          backgroundColor: theme.palette.secondary.dark,
+        },
+  spacer: {
+    flex: '1 1 100%',
+  },
+  actions: {
+    color: theme.palette.text.secondary,
+  },
+  title: {
+    flex: '0 0 auto',
+  },
+});
+
+let EnhancedTableToolbar = props => {
+  const {  classes } = props;
+
+  return (
+    <Toolbar
+      className={classNames(classes.root, {
+        //[classes.highlight]: numSelected > 0,
+      })}
+    >
+      <div className={classes.title}>
+        {
+        //   numSelected > 0 ? (
+        //   <Typography color="inherit" variant="subtitle1">
+        //     {numSelected} selected
+        //   </Typography>
+        // ) :  
+        (
+          <Typography variant="h6" id="tableTitle">
+            Product Growth
+          </Typography>
+        )}
+      </div>
+      <div className={classes.spacer} />
+      {/* <div className={classes.actions}>
+        {numSelected > 0 ? (
+          <Tooltip title="Delete">
+            <IconButton aria-label="Delete">
+              <DeleteIcon />
+            </IconButton>
+          </Tooltip>
+        ) : (
+          <Tooltip title="Filter list">
+            <IconButton aria-label="Filter list">
+              <FilterListIcon />
+            </IconButton>
+          </Tooltip>
+        )}
+      </div> */}
+    </Toolbar>
+  );
+};
+
+EnhancedTableToolbar.propTypes = {
+  classes: PropTypes.object.isRequired,
+  numSelected: PropTypes.number.isRequired,
+};
+
+EnhancedTableToolbar = withStyles(toolbarStyles)(EnhancedTableToolbar);
 
 const styles = theme => ({
   root: {
-    width: '80%',
+    width: '100%',
     marginTop: theme.spacing.unit * 3,
     overflowX: 'auto',
   },
   table: {
-    minWidth: 400,
+    minWidth: 700,
+  },
+  row: {
+        '&:nth-of-type(odd)': {
+          backgroundColor: theme.palette.background.default,
+        },
+      },
+  tableWrapper: {
+    overflowX: 'auto',
   },
 });
 
+class EnhancedTable extends React.Component {
+  state = {
+    order: 'asc',
+    orderBy: 'PlanType',
+    selected: [],
+    data: mockTableData,
+    page: 0,
+    rowsPerPage: 5,
+  };
 
-function SimpleTable(props) {
-  const { classes } = props;
+  handleRequestSort = (event, property) => {
+    const orderBy = property;
+    let order = 'desc';
 
-  return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell><b>OrganizationName</b></TableCell>
-            <TableCell align="right"><b> PlanType</b></TableCell>
-            <TableCell align="right"><b>Product</b></TableCell>
-            <TableCell align="right"><b>Segment</b></TableCell>
-            <TableCell align="right"><b>Geography</b></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {mockTableData.map(row => (
-            <TableRow key={row.id}>
-              <TableCell component="th" scope="row">
-                {row.OrganizationName}
-              </TableCell>
-              <TableCell align="right">{row.PlanType}</TableCell>
-              <TableCell align="right">{row.Product}</TableCell>
-              <TableCell align="right">{row.Segment}</TableCell>
-              <TableCell align="right">{row.Geography}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Paper>
-  );
+    if (this.state.orderBy === property && this.state.order === 'desc') {
+      order = 'asc';
+    }
+
+    this.setState({ order, orderBy });
+  };
+
+  handleSelectAllClick = event => {
+    if (event.target.checked) {
+      this.setState(state => ({ selected: state.data.map(n => n.id) }));
+      return;
+    }
+    this.setState({ selected: [] });
+  };
+
+  handleClick = (event, id) => {
+    const { selected } = this.state;
+    const selectedIndex = selected.indexOf(id);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, id);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1),
+      );
+    }
+
+    this.setState({ selected: newSelected });
+  };
+
+  handleChangePage = (event, page) => {
+    this.setState({ page });
+  };
+
+  handleChangeRowsPerPage = event => {
+    this.setState({ rowsPerPage: event.target.value });
+  };
+
+  isSelected = id => this.state.selected.indexOf(id) !== -1;
+
+  render() {
+    const { classes } = this.props;
+    const { data, order, orderBy, selected, rowsPerPage, page } = this.state;
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, data - page * rowsPerPage);
+
+    return (
+      <Paper className={classes.root}>
+        <EnhancedTableToolbar numSelected={selected.length} />
+        <div className={classes.tableWrapper}>
+          <Table className={classes.table} aria-labelledby="tableTitle">
+            <EnhancedTableHead
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={this.handleSelectAllClick}
+              onRequestSort={this.handleRequestSort}
+              rowCount={data.length}
+            />
+            <TableBody>
+              {stableSort(data, getSorting(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map(n => {
+                  const isSelected = this.isSelected(n.id);
+                  return (
+                    <TableRow
+                      //hover
+                      //onClick={event => this.handleClick(event, n.id)}
+                      //role="checkbox"
+                      //aria-checked={isSelected}
+                      //tabIndex={-1}
+                      //key={n.id}
+                      //selected={isSelected}
+                    >
+                      <TableCell padding="checkbox">
+                        {/* <Checkbox checked={isSelected} /> */}
+                      </TableCell>
+                      <CustomTableCell align="left">{n.OrganizationName}</CustomTableCell>
+                      <CustomTableCell align="left">{n.PlanType}</CustomTableCell>
+                      <CustomTableCell align="left">{n.Product}</CustomTableCell>
+                      <CustomTableCell align="left">{n.Segment}</CustomTableCell>
+                      <CustomTableCell align="left">{n.Geography}</CustomTableCell>
+                    </TableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 49 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={data.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          backIconButtonProps={{
+            'aria-label': 'Previous Page',
+          }}
+          nextIconButtonProps={{
+            'aria-label': 'Next Page',
+          }}
+          onChangePage={this.handleChangePage}
+          onChangeRowsPerPage={this.handleChangeRowsPerPage}
+        />
+      </Paper>
+    );
+  }
 }
 
-SimpleTable.propTypes = {
+EnhancedTable.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(SimpleTable);
-//export default Table;
+export default withStyles(styles)(EnhancedTable);
--- src/mockTableData.js
+++ src/mockTableData.js
@@ -1,6 +1,495 @@
 export default [
     {
-        OrganizationName: 'Frozent',
+        OrganizationName: 'Antena',
+        PlanType: 'MAPD',
+        Product: 'RPPO',
+        Segment:  24,
+        Geography: 'Lee'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'Antena',
+        PlanType: 'MAPD',
+        Product: 'RPPO',
+        Segment:  24,
+        Geography: 'Lee'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'Slice',
+        PlanType: 'LPPO',
+        Product: 'MAPD',
+        Segment:  24,
+        Geography: 'Plam Beach'
+    },
+    {
+        OrganizationName: 'Slice',
+        PlanType: 'LPPO',
+        Product: 'MAPD',
+        Segment:  24,
+        Geography: 'Plam Beach'
+    },
+    {
+        OrganizationName: 'Slice',
+        PlanType: 'LPPO',
+        Product: 'MAPD',
+        Segment:  24,
+        Geography: 'Plam Beach'
+    },
+    {
+        OrganizationName: 'Slice',
+        PlanType: 'LPPO',
+        Product: 'MAPD',
+        Segment:  24,
+        Geography: 'Plam Beach'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'Slice',
+        PlanType: 'LPPO',
+        Product: 'MAPD',
+        Segment:  24,
+        Geography: 'Plam Beach'
+    }, {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'Slice',
+        PlanType: 'LPPO',
+        Product: 'MAPD',
+        Segment:  24,
+        Geography: 'Plam Beach'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+    OrganizationName: 'Slice',
+    PlanType: 'LPPO',
+    Product: 'MAPD',
+    Segment:  24,
+    Geography: 'Plam Beach'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'Antena',
+        PlanType: 'MAPD',
+        Product: 'RPPO',
+        Segment:  24,
+        Geography: 'Lee'
+    },
+    {
+        OrganizationName: 'Slice',
+        PlanType: 'LPPO',
+        Product: 'MAPD',
+        Segment:  24,
+        Geography: 'Plam Beach'
+    },
+    {
+        OrganizationName: 'Antena',
+        PlanType: 'MAPD',
+        Product: 'RPPO',
+        Segment:  24,
+        Geography: 'Lee'
+    },
+    {
+        OrganizationName: 'Antena',
+        PlanType: 'MAPD',
+        Product: 'RPPO',
+        Segment:  24,
+        Geography: 'Lee'
+    },
+    {
+        OrganizationName: 'Antena',
+        PlanType: 'MAPD',
+        Product: 'RPPO',
+        Segment:  24,
+        Geography: 'Lee'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'Antena',
+        PlanType: 'MAPD',
+        Product: 'RPPO',
+        Segment:  24,
+        Geography: 'Lee'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'Antena',
+        PlanType: 'MAPD',
+        Product: 'RPPO',
+        Segment:  24,
+        Geography: 'Lee'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'Antena',
+        PlanType: 'MAPD',
+        Product: 'RPPO',
+        Segment:  24,
+        Geography: 'Lee'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'Antena',
+        PlanType: 'MAPD',
+        Product: 'RPPO',
+        Segment:  24,
+        Geography: 'Lee'
+    },
+    {
+        OrganizationName: 'Antena',
+        PlanType: 'MAPD',
+        Product: 'RPPO',
+        Segment:  24,
+        Geography: 'Lee'
+     
+    },
+    {
+        OrganizationName: 'Antena',
+        PlanType: 'MAPD',
+        Product: 'RPPO',
+        Segment:  24,
+        Geography: 'Lee'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    }, {
+        OrganizationName: 'test',
+        PlanType: 'HMO',
+        Product: 'PDP',
+        Segment:  24,
+        Geography: 'Broward'
+    },
+    {
+        OrganizationName: 'test',
         PlanType: 'HMO',
         Product: 'PDP',
         Segment:  24,
